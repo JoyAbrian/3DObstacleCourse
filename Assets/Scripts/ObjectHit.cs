fd2bd6c9c575ca405ef9bbb302487be4732d9d6b
anisanhy <joyabrianrantepasang14@gmail.com>
2024-07-01 23:28:20 +0800

    add more obstacle

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
+    [SerializeField] Material afterHitMaterial;
+
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Player") {
-            GetComponent<MeshRenderer>().material.color = Color.red;
+            GetComponent<MeshRenderer>().material = afterHitMaterial;
             gameObject.tag = "Hit";
         }
     }
