2328a8210ae1c0f22bc4433b22a886c4b2174eaa
anisanhy <joyabrianrantepasang14@gmail.com>
2024-07-01 13:45:43 +0800

    player movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    void Start() {
+        
+    }
+
+    void Update() {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime , 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
